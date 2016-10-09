77a50f66bbc882a10e7e2f0a2569e4033c707e62
Sych Yuriy <sichyuriyy@gmail.com>
2016-10-09 16:18:57 +0300

    ConsoleScanner added

---
+++ Lab3/Lab3/ConsoleScanner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class ConsoleScanner
+    {
+
+        private static ConsoleScanner consoleScanner;
+
+        private static List<int> arr;
+        private static int tempIndex;
+        private static bool endOfLine = true;
+
+        public static ConsoleScanner GetInstance()
+        {
+            if (consoleScanner == null)
+            {
+                consoleScanner = new ConsoleScanner();
+            }
+            return consoleScanner;
+        }
+
+        private ConsoleScanner()
+        {
+            arr = new List<int>();
+        }
+
+        public int NextInt()
+        {
+            if (endOfLine)
+            {
+                tempIndex = 0;
+                arr.Clear();
+                while (arr.Count == 0)
+                {
+                    string str = Console.ReadLine();
+                    string[] inputs = str.Split(' ');
+                    for (int i = 0; i < inputs.Length; i++)
+                    {
+                        int val;
+                        if (int.TryParse(inputs[i], out val))
+                        {
+                            arr.Add(val);
+                        }
+                    }
+                }
+            }
+            int result = arr[tempIndex++];
+            if (tempIndex == arr.Count)
+            {
+                endOfLine = true;
+            }
+
+            return result;
+        }
+
+    }
+
+    
+}
